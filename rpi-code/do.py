68e5b887368183393a3064209c50985d13185108
jerenz pellina <pellina01@gmail.com>
2021-01-05 15:59:18 +0800

    correcting some wrong syntax

--- rpi-code/do.py
+++ rpi-code/do.py
@@ -2,7 +2,7 @@ from wire1 import read_value
 import math
 
 
-def read_do(*args):
+def read_do(self, *args):
     temp = [
         0,
         1,
@@ -105,7 +105,7 @@ def read_do(*args):
         5.79
     ]
     try:
-        status , value = read_value()
+        status, value = read_value()
         if status == "error":
             return status, value
 
@@ -113,15 +113,16 @@ def read_do(*args):
             return "error", "Cannot interpolate. Limit reached"
         elif value in temp:
             index = temp.index(value)
-            return "ok" ,d_o[index]
+            return "ok", d_o[index]
         else:
             max_index = temp.index(math.trunc(value))
             min_index = max_index + 1
             yout = d_o[min_index] + ((value - temp[min_index]) * (
                 d_o[max_index] - d_o[min_index]) / (temp[max_index] - temp[min_index]))
-            return "ok" ,round(yout, 2)
+            return "ok", round(yout, 2)
     except Exception as e:
         return "error", e
+
 
 if __name__ == "__main__":
     print(read_do())
--- rpi-code/wire1.py
+++ rpi-code/wire1.py
@@ -2,14 +2,14 @@ from w1thermsensor import W1ThermSensor
 import time
 
 
-def read_value(*args):
-	try:
-	    sensor = W1ThermSensor()
-	    temperature_in_celsius = sensor.get_temperature()
-	    del sensor
-	    return "ok" ,temperature_in_celsius
-	except Exception as e:
-		return "error", e
+def read_value(self, *args):
+    try:
+        sensor = W1ThermSensor()
+        temperature_in_celsius = sensor.get_temperature()
+        del sensor
+        return "ok", temperature_in_celsius
+    except Exception as e:
+        return "error", e
 
 
 if __name__ == "__main__":
