dfe12a80174e5358018f1f99592d9440374a3d8b
jerenz pellina <pellina01@gmail.com>
2021-01-07 15:56:56 +0800

    add aggregator

---
+++ instance-code/aggregate.py
@@ -0,0 +1,13 @@
+import json
+from aggregator import aggregator
+
+with open('config.json', 'r') as file:
+    data = json.loads(file.read())
+
+cloud = {}
+for key, value in data["cloud"].items():
+    cloud.update({key: value})
+
+for topic in cloud["topics_aggregated"]:
+    aggregator(topic, cloud["influxHost"], cloud["username"],
+               cloud["password"], cloud["database_aggregated"]).aggregate()
---
+++ instance-code/aggregator.py
@@ -0,0 +1,44 @@
+
+class aggregator:
+    from influxdb import InfluxDBClient
+    import time
+    import math
+    import json
+
+    def __init__(self, topic, host, user, pw, db):
+        self.topic = topic
+        self.client = self.InfluxDBClient(host=host, port=8086,
+                                          username=user, password=pw)
+        self.client.switch_database(db)
+
+    def aggregate(self):
+        try:
+            query_result = list(self.client.query(
+                'SELECT * FROM {} WHERE time > now() - 1d'.format(self.topic)).get_points(measurement=self.topic))
+
+            data_points = list(query_result.get_points(measurement=self.topic))
+            self.aggregated_data = 0
+            n = 0
+            for lists in data_points:
+                self.aggregated_data += lists["value"]
+                n += 1
+            self.aggregated_data /= n
+
+            json_body = __serializer()
+
+            self.client.write_points(json_body)
+        except Exception as e:
+            print(e)
+
+    def __serializer(self):
+        return [
+            {
+                "measurement": "{}_aggregated".format(self.topic),
+                "tags": {
+                    "user": self.topic,
+                },
+                "time": self.math.trunc(time.time())-86400,
+                "fields": {
+                    "value": aggregated_data
+                }
+            }]
