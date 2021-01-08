a388bb32d26c2cffd6cd3491f22d6a58e828ccb5
unknown <pellina01@gmail.com>
2021-01-08 14:57:13 +0800

    change do to temp

--- rpi-code/raspi.py
+++ rpi-code/raspi.py
@@ -9,6 +9,9 @@ time.sleep(5)
 with open('config.json', 'r') as file:
     data = json.loads(file.read())
 
+with open("sample2.txt", "w") as text_file:
+    text_file.write("python starts")
+
 raspi = {}
 for key, value in data["raspi"].items():
     raspi.update({key: value})
