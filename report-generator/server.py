28779fdb3d66f5071d6275ec8c1dd6c50387ab23
pellina <pellina01@gmail.com>
2021-02-08 20:05:50 +0800

    added umage rendering

--- report-generator/server.py
+++ report-generator/server.py
@@ -1,10 +1,31 @@
-from flask import Flask, make_response, render_template
+from flask import Flask, make_response, render_template, send_file
 import pdfkit
+from matplotlib import pyplot as plt 
 
 app = Flask(__name__)
 
+def plot():
+    x = [1,2,3] 
+    y = [2,4,1] 
+    # plotting the points  
+    plt.plot(x, y) 
+    # naming the x axis 
+    plt.xlabel('x - axis') 
+    # naming the y axis 
+    plt.ylabel('y - axis') 
+    # giving a title to my graph 
+    plt.title('My first graph!') 
+    # function to show the plot 
+    plt.savefig('report-generator/templates/psample.png')
+
+@app.route('/img/psample')
+def psample():
+    return send_file('templates/psample.png')
+
+
 @app.route('/<name>')
 def main(name):
+    plot()
     rendered = render_template('hello-world.html',name=name) #embedded jinja2 on flask default directory is templates/ . there is no need to indicate to the path
     pdf = pdfkit.from_string(rendered, False)
 
@@ -13,6 +34,7 @@ def main(name):
     response.headers['Content-Disposition'] = 'inline; filename=output.pdf'
 
     return response
+    # return rendered
 
 if __name__ == "__main__":
     app.run()
