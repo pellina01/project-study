0d340fedaeb0e59428261b7f4976c705d6eb995f
pellina <pellina01@gmail.com>
2021-02-12 05:38:49 +0800

    handle no data return

--- report-generator/chart_dbquery_handler.py
+++ report-generator/chart_dbquery_handler.py
@@ -2,35 +2,48 @@ class chart:
     from matplotlib import pyplot as plt 
     from influxdb import InfluxDBClient
 
-    def __init__(self, database, measurement, username, password, influxHost, influxPort=8086):
+    def __init__(self, measurement, database, username, password, influxHost, influxPort=8086):
         self.measurement = measurement
+        self.has_data = False
 
         self.influxClient = self.InfluxDBClient(
             influxHost, influxPort, username, password)
         self.influxClient.switch_database(database)
 
     def query_db(self, frm, to):
-        self.data = self.influxClient.query(
-            'SELECT * FROM {measurement} WHERE time > {time}'.format(
-                measurement=self.measurement,time=to-frm))
-        self.datapoints = self.data.get_points(measurement=self.measurement)
+        try:
+            self.data = self.influxClient.query(
+                'SELECT * FROM {measurement} WHERE time > {time}'.format(
+                    measurement=self.measurement,time=to-frm))
+            self.datapoints = self.data.get_points(measurement=self.measurement)
 
-        self.time = []
-        self.amplitude = []
+            self.time = []
+            self.amplitude = []
 
-        for point in self.datapoints:
-            self.time.append(point['time'])
-            self.amplitude.append(['value'])
+            for point in self.datapoints:
+                self.time.append(point['time'])
+                self.amplitude.append(['value'])
+            self.has_data = True
+        except:
+            self.has_data = False
 
     def generate_plot(self):
 
-        self.plt.plot(self.time, self.amplitude) 
-        self.plt.gca.set_xlim([min(self.time),max(self.time)])
-        self.plt.gca.set_ylim([min(self.amplitude),max(self.amplitude)])
-        self.plt.xlabel('time') 
-        self.plt.ylabel('{} value'.format(self.measurement)) 
-        self.plt.title(self.measurement) 
-        self.plt.savefig('report-generator/images/{}.png'.format(self.measurement))
+        if self.has_data:
+            self.plt.plot(self.time, self.amplitude) 
+            self.plt.gca.set_xlim([min(self.time),max(self.time)])
+            self.plt.gca.set_ylim([min(self.amplitude),max(self.amplitude)])
+            self.plt.xlabel('time') 
+            self.plt.ylabel('{} value'.format(self.measurement)) 
+            self.plt.title(self.measurement) 
+            self.plt.savefig('report-generator/images/{}.png'.format(self.measurement))
+            self.image_link = 'report-generator/images/{}.png'.format(self.measurement)
+
+        else:
+            self.image_link = 'report-generator/images/no_data.png'
+
 
     def get_plot_directory(self):
-        return 'report-generator/images/{}.png'.format(self.measurement)
+        del self.time 
+        del self.amplitude 
+        return self.image_link
