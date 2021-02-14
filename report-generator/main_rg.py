aacbe7c498c8efbcc641acdc4e148e8c2d8cc2e8
pellina01 <pellina01@github.com>
2021-02-14 09:53:30 +0800

    edit plot

--- report-generator/main_rg.py
+++ report-generator/main_rg.py
@@ -19,7 +19,7 @@ def api(frm, to, title):
         sensor_objs[sensor].generate_plot(to, frm)
 
     variables = {
-        'frm': datetime.utcfromtimestamp(int(frm).strftime('%Y-%m-%dT%H:%M:%SZ'),
+        'frm': datetime.utcfromtimestamp(int(frm)).strftime('%Y-%m-%dT%H:%M:%SZ'),
         'to': datetime.utcfromtimestamp(int(to)).strftime('%Y-%m-%dT%H:%M:%SZ'),
         'title': title,
         'sensors': sensors
