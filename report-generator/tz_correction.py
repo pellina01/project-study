302c235875482287cc901bcff7d0b10d842fc2a4
pellina01 <pellina01@github.com>
2021-02-14 13:26:23 +0800

    edit plot

--- report-generator/tz_correction.py
+++ report-generator/tz_correction.py
@@ -3,7 +3,7 @@ import pytz
 
 timezone = pytz.timezone("Asia/Manila")
 
-def tz_correction(time_in_z):
+def tz_correction(self, time_in_z):
     utc = datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
     datetime_manila = utc.astimezone(timezone)
     return datetime_manila
