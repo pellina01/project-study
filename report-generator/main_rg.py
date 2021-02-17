69950b611f9bca7e104a5b83ad780b4d2f954827
pellina01 <pellina01@github.com>
2021-02-17 13:35:23 +0800

    edit css

--- report-generator/main_rg.py
+++ report-generator/main_rg.py
@@ -18,8 +18,8 @@ def api():
     utc_to = request.args.get('to')
     title = request.args.get('title')
 
-    for sensor in sensors:
-        sensor_objs[sensor].generate_plot(utc_frm, utc_to)
+    for key in sensors:
+        sensor_objs[key].generate_plot(utc_frm, utc_to)
 
     frm = tz_corrector.set_to_ph(utc_frm)
     to = tz_corrector.set_to_ph(utc_to)
@@ -49,14 +49,14 @@ if __name__ == "__main__":
 
     with open('/home/ubuntu/project-study/report-generator/config.json', 'r') as file:
         data = json.loads(file.read())
-    sensors = ["ph", "tb", "temp", "do"]
+    sensors = {"ph":"pH", "tb":"turbidity", "temp":"temperature", "do":"dissolved oxygen"}
     sensor_objs = {}
 
     tz_corrector = tz("Asia/Manila")
 
-    for sensor in sensors:
-        sensor_objs[sensor] = chart(
-            sensor, dbase(tz_corrector, sensor, data['cloud']['database'],
+    for key in sensors:
+        sensor_objs[key] = chart(
+            key, dbase(tz_corrector, key, data['cloud']['database'],
                           data['cloud']['username'], data['cloud']['password'],
                           data['cloud']['influxHost']))
     reporter_host = "3.236.45.125:5000"
