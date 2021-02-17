196563de9e7e5bfc97df382f5b035c730b6aa8bf
pellina01 <pellina01@github.com>
2021-02-17 20:45:54 +0800

    modify plot

--- report-generator/tz_correction.py
+++ report-generator/tz_correction.py
@@ -9,3 +9,10 @@ class tz_correction:
 	    utc = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
 	    datetime_manila = utc.astimezone(self.timezone)
 	    return datetime_manila.strftime('%Y-%m-%d %I:%M %p')
+
+    def get_datetime_obj(self, frm, to):
+	    new_frm = self.datetime.strptime(frm, '%Y-%m-%dT%H:%M:%S.%fZ')
+	    new_frm = new_frm.astimezone(self.timezone)
+	    new_to = self.datetime.strptime(frm, '%Y-%m-%dT%H:%M:%S.%fZ')
+	    new_to = new_to.astimezone(self.timezone)
+	    return new_frm.strftime('%Y-%m-%d %I:%M %p'), new_to.strftime('%Y-%m-%d %I:%M %p')
