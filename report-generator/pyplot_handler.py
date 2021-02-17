9fa93a6ee72bd3d26451d21b7efbf33207c7c634
pellina01 <pellina01@github.com>
2021-02-17 17:41:35 +0800

    edit center image

--- report-generator/pyplot_handler.py
+++ report-generator/pyplot_handler.py
@@ -20,7 +20,7 @@ class chart:
                 self.plt.ylabel('{} value'.format(self.measurement))
                 self.plt.title(self.measurement)
                 self.plt.tight_layout()
-                self.plt.figure(figsize=(10, 6))
+                self.plt.figure(figsize=(2, 2))
                 self.plt.savefig(
                     '/home/ubuntu/project-study/report-generator/static/images/{}.png'.format(self.measurement))
                 self.image_link = '/home/ubuntu/project-study/report-generator/static/images/{}.png'.format(
