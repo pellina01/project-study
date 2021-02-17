3b742ee0d7e3077241e6dcb1e64fd5ea4839c053
pellina01 <pellina01@github.com>
2021-02-17 13:59:15 +0800

    edit css

--- report-generator/tz_correction.py
+++ report-generator/tz_correction.py
@@ -6,7 +6,6 @@ class tz_correction:
 		self.timezone = self.pytz.timezone(timezone)
 
 	def set_to_ph(self, time_in_z):
-		dt = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
-		utc = self.datetime(dt.year, dt.month, dt.day, dt.hour, dt.minute)
-		datetime_manila = utc.astimezone(self.timezone)
-		return datetime_manila
+	    dt = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
+	    utc = self.datetime.strftime(dt, '%Y-%m-%d %H:%MZ')
+	    return utc.astimezone(self.timezone)
