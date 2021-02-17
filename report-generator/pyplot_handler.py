4bea0dc5b5b91d246a2b30bd0f95a2b7f8f35a28
pellina01 <pellina01@github.com>
2021-02-17 19:13:39 +0800

    modify plot

--- report-generator/pyplot_handler.py
+++ report-generator/pyplot_handler.py
@@ -15,7 +15,7 @@ class chart:
             self.time, self.amplitude = self.dbase.query(frm, to)
             print(self.measurement, self.time, self.amplitude)
             if len(self.time) > 0:
-                self.plt.figure().set_size_inches(12,10)
+                self.plt.figure().set_size_inches(4,3)
                 self.plt.plot_date(self.time, self.amplitude)
                 self.plt.xlabel('time')
                 self.plt.ylabel('{} value'.format(self.measurement))
