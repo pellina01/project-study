9ca298b369daefc9f6f78978aec81fa58dc43eef
pellina01 <pellina01@github.com>
2021-02-17 19:44:04 +0800

    modify plot

--- report-generator/pyplot_handler.py
+++ report-generator/pyplot_handler.py
@@ -1,32 +1,32 @@
 class chart:
+    from matplotlib import pyplot as plt
+    from matplotlib import dates as mpl_dates
+    import traceback
+
     def __init__(self, measurement, dbase):
 
-        from matplotlib import pyplot as plt
-        from matplotlib import dates as mpl_dates
-        import traceback
-
-        self.plt = plt
-        self.mpl_dates = mpl_dates
-        self.traceback = traceback
+        # self.plt = plt
+        # self.mpl_dates = mpl_dates
+        # self.traceback = traceback
 
         self.dbase = dbase
         self.measurement = measurement
         self.no_data = '/home/ubuntu/project-study/report-generator/static/images/no_data.png'
         self.image_link = self.no_data
         self.generated = False
-        self.plt.style.use('seaborn')
-        self.plt.figure().set_size_inches(4,2)
-        self.plt.xlabel('time')
-        self.plt.tight_layout()
 
     def generate_plot(self, frm, to):
         try:
             self.time, self.amplitude = self.dbase.query(frm, to)
             print(self.measurement, self.time, self.amplitude)
             if len(self.time) > 0:
-                self.plt.plot_date(self.time, self.amplitude)
+                self.plt.style.use('seaborn')
+                self.plt.figure().set_size_inches(4,2)
+                self.plt.xlabel('time')
+                self.plt.tight_layout()
                 self.plt.ylabel('{} value'.format(self.measurement))
                 self.plt.title(self.measurement)
+                self.plt.plot_date(self.time, self.amplitude)
                 self.plt.savefig(
                     '/home/ubuntu/project-study/report-generator/static/images/{}.png'.format(self.measurement), 
                     dpi=100)
