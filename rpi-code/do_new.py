8db6d0d7426c2f06b960429242f7f987b8556491
unknown <pellina01@gmail.com>
2021-02-19 17:11:23 +0800

    added new do program

--- rpi-code/do_new.py
+++ rpi-code/do_new.py
@@ -8,12 +8,11 @@ cal2_t = 21 #℃
 
 resolution = 1024 #level
 reference = 5000 #mv
-convert = 1000 #mV per V
 
-do_table = [ 14460, 14220, 13820, 13440, 13090, 12740, 12420, 12110, 11810, 11530,
-            11260, 11010, 10770, 10530, 10300, 10080, 9860, 9660, 9460, 9270, 
-            9080, 8900, 8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,   
-            7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730, 6630, 6530, 6410]
+do_table = [ 14.460, 14.220, 13.820, 13.440, 13.090, 12.740, 12.420, 12.110, 11.810, 11.530,
+            11.260, 11.010, 10.770, 10.530, 10.300, 10.080, 9.860, 9.660, 9.460, 9.270, 
+            9.080, 8.900, 8.730, 8.570, 8.410, 8.250, 8.110, 7.960, 7.820, 7.690,   
+            7.560, 7.430, 7.300, 7.180, 7.070, 6.950, 6.840, 6.730, 6.630, 6.530, 6.410]
 
 def read_do(slave_addr, sensor_type):
     try:
@@ -24,6 +23,6 @@ def read_do(slave_addr, sensor_type):
         rounded_temp = round(temp[1])
         V_saturation = (temp[1] - cal2_t) *(cal1_v - cal2_v) / (cal1_t - cal2_t) + cal2_v
         print("do not converted: " , str(adc_voltage * do_table[rounded_temp] / V_saturation))
-        return "ok", (adc_voltage * do_table[rounded_temp] / (V_saturation * convert))
+        return "ok", round(adc_voltage * do_table[rounded_temp] / V_saturation, 2)
     except Exception as e:
         return "error", e
