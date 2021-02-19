aea9174afa7a8b7760e3ea4d796a99cf036f4ca5
jerenz pellina <pellina01@gmail.com>
2021-02-19 20:52:08 +0800

    edit DO feedback

--- rpi-code/feedback.py
+++ rpi-code/feedback.py
@@ -5,8 +5,8 @@ class feedback:
 
     def __init__(self, mqtt_url, sensor_function, corrective_function, higher_limit, lower_limit, device_name):
         self.device = device_name
-        self.HL = higher_limit
-        self.LL = lower_limit
+        self.higher_limit = higher_limit
+        self.lower_limit = lower_limit
         self.correction = corrective_function
         self.read_sensor_value = sensor_function
 
@@ -15,11 +15,14 @@ class feedback:
         self.mq_client = self.mqtt(device_name, mqtt_url)
         self.prev_status = False
 
+        print("successful establishing connection with mqtt")
+
     def check(self):
         print(self.read_sensor_value()[1])
-        if self.LL > self.read_sensor_value()[1] or self.read_sensor_value()[1] > self.HL:
+        sensor_val = self.read_sensor_value()[1]
+        if self.lower_limit > sensor_val or sensor_val > self.higher_limit:
             self.feedback_is_on = True
-        else: 
+        else:
             self.feedback_is_on = False
 
         if not self.sent or self.feedback_is_on != self.prev_status:
@@ -29,20 +32,16 @@ class feedback:
             if self.mq_client.send(self.__feedback_serializer()):
                 self.sent = True
             # else:
-                
 
-        print("feedback is on: " + str(self.feedback_is_on))
+        print("feedback status: " + str(self.feedback_is_on))
         print("sent: " + str(self.sent))
         return 300
 
-
     def __feedback_serializer(self):
         return self.json.dumps({
-                "status": "ok", 
-                "value": "{} is {}".format(self.device,"on" if self.feedback_is_on else "off")
-                })
- 
-
+            "status": "ok",
+            "value": "{} is {}".format(self.device, "on" if self.feedback_is_on else "off")
+        })
 
 
 def serialize(read, address, cmd_on, cmd_off):
@@ -66,16 +65,12 @@ if __name__ == "__main__":
     for key, value in data["raspi"].items():
         raspi.update({key: value})
 
-    function = serialize(read_arduino, 11, 3, 4)
-    aerator = feedback(raspi["mqtt_url"], read_do, function, 8.25, 7.56, "rpi")
+    switch = serialize(read_arduino, 11, 4, 5)
+    aerator = feedback(raspi["mqtt_url"], read_do,
+                       switch, 8.25, 7.56, "aerator")
     while True:
         try:
             delay = aerator.check()
             time.sleep(delay)
         except Exception as e:
             print(e)
-
-
-
-
-
--- rpi-code/main_rc.py
+++ rpi-code/main_rc.py
@@ -17,6 +17,7 @@ raspi = {}
 for key, value in data["raspi"].items():
     raspi.update({key: value})
 
+
 def main():
     logging.basicConfig(filename="error.log")
 
@@ -27,18 +28,21 @@ def main():
         print(traceback.format_exc())
         logging.error(traceback.format_exc())
 
+
 def sched(mins):
     hour = math.trunc(mins/60)
     minute = mins - (60*hour)
     formatted_hour = "0{}".format(hour) if hour < 10 else hour
     formatted_minute = "0{}".format(minute) if minute < 10 else minute
-    return "{}:{}".format(formatted_hour,formatted_minute)
+    return "{}:{}".format(formatted_hour, formatted_minute)
 
-#schedule.every(1).minutes.do(main)
+
+# schedule.every(1).minutes.do(main)
 schedules = []
 period = math.trunc(1440/raspi["schedule_per_day"])
 for i in range(0, raspi["schedule_per_day"]):
-    print(schedules.append(schedule.every().day.at(sched(period*i)).do(main)))
+    schedules.append(schedule.every().day.at(sched(period*i)).do(main))
+    print("done scheduling time at: ", sched(period*i))
 
 # rpi = mqtt("rpi", raspi["mqtt_url"])
 
