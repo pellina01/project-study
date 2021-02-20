ec9bb0cfd10a00981fd333e609dee65397b98bce
jerenz pellina <pellina01@gmail.com>
2021-02-20 13:29:27 +0800

    rotate tick

--- report-generator/pyplot_handler.py
+++ report-generator/pyplot_handler.py
@@ -18,7 +18,7 @@ class chart:
             if len(self.time) > 0:
                 print("frm_dt: ", frm_dt)
                 print("to_dt", to_dt)
-                self.plt.plot_datetime(self.time, self.amplitude)
+                self.plt.plot_date(self.time, self.amplitude)
                 self.plt.xlim([frm_dt, to_dt])
                 self.plt.figure().set_size_inches(3, 2)
                 self.plt.title(self.measurement)
