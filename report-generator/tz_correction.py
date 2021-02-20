5e1a47984ad96f44db210b21d38236df547fb853
jerenz pellina <pellina01@gmail.com>
2021-02-20 14:22:51 +0800

    rotate tick

--- report-generator/tz_correction.py
+++ report-generator/tz_correction.py
@@ -1,28 +1,20 @@
 class tz_correction:
-	from datetime import datetime, timezone
-	import pytz
+    from datetime import datetime, timezone
+    import pytz
 
-	def __init__(self, timezone):
-		self.tzone = self.pytz.timezone(timezone)
+    def __init__(self, timezone):
+        self.tzone = self.pytz.timezone(timezone)
 
-	def get_string(self, time_in_z):
-		unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
-		utc_aware = unaware.replace(tzinfo=self.pytz.utc)
-		tz_aware = utc_aware.astimezone(self.tzone)
+    def get_string(self, time_in_z):
+        unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
+        utc_aware = unaware.replace(tzinfo=self.pytz.utc)
+        tz_aware = utc_aware.astimezone(self.tzone)
 
-		return tz_aware.strftime('%Y-%m-%d %I:%M %p')
+        return tz_aware.strftime('%Y-%m-%d %I:%M %p')
 
-	# def get_datetime_obj(self, frm, to):
-	#     new_frm = self.datetime.strptime(frm, '%Y-%m-%dT%H:%M:%S.%fZ')
-	#     new_frm = new_frm.astimezone(self.timezone)
-	#     new_to = self.datetime.strptime(frm, '%Y-%m-%dT%H:%M:%S.%fZ')
-	#     new_to = new_to.astimezone(self.timezone)
-	#     return new_frm.strftime('%Y-%m-%d %I:%M %p'), new_to.strftime('%Y-%m-%d %I:%M %p')
+    def get_datetime(self, time_in_z):
+        unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
+        utc_aware = unaware.replace(tzinfo=self.pytz.utc)
+        tz_aware = utc_aware.astimezone(self.tzone)
 
-	def get_datetime(self, time_in_z):
-		unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
-		utc_aware = unaware.replace(tzinfo=self.pytz.utc)
-		tz_aware = utc_aware.astimezone(self.tzone)
-
-		return tz_aware
-
+        return tz_aware
