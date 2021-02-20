38ac55f1bf581e22b3d8726d3ee9b5750aebc8bb
jerenz pellina <pellina01@gmail.com>
2021-02-20 13:30:39 +0800

    rotate tick

--- report-generator/pyplot_handler.py
+++ report-generator/pyplot_handler.py
@@ -28,7 +28,7 @@ class chart:
                 self.plt.savefig(
                     '/home/ubuntu/project-study/report-generator/static/images/{}.png'.format(
                         self.measurement),
-                    dpi=50)
+                    dpi=125)
                 self.image_link = '/home/ubuntu/project-study/report-generator/static/images/{}.png'.format(
                     self.measurement)
             else:
