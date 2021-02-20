59f08e247425e22096048603f2ddba0b8472caf0
pellina01 <pellina01@github.com>
2021-02-20 11:54:22 +0800

    datetime edited

--- report-generator/tz_correction.py
+++ report-generator/tz_correction.py
@@ -6,10 +6,9 @@ class tz_correction:
 		self.tzone = self.pytz.timezone(timezone)
 
 	def get_string(self, time_in_z):
-		tz_py = self.pytz.timezone(self.tzone)
 		unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%MZ')
 		utc_aware = unaware.replace(tzinfo=self.pytz.utc)
-		tz_aware = utc_aware.astimezone(tz_py)
+		tz_aware = utc_aware.astimezone(self.tzone)
 
 		return tz_aware.strftime('%Y-%m-%d %I:%M %p')
 
@@ -21,10 +20,9 @@ class tz_correction:
 	#     return new_frm.strftime('%Y-%m-%d %I:%M %p'), new_to.strftime('%Y-%m-%d %I:%M %p')
 
 	def get_datetime(self, time_in_z):
-		tz_py = self.pytz.timezone(self.tzone)
 		unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%MZ')
 		utc_aware = unaware.replace(tzinfo=self.pytz.utc)
-		tz_aware = utc_aware.astimezone(tz_py)
+		tz_aware = utc_aware.astimezone(self.tzone)
 
 		return tz_aware
 
