0637fe971a6b63cc8627b09da94d6263973f04da
jerenz pellina <pellina01@gmail.com>
2021-02-21 17:11:52 +0800

    new table added

--- report-generator/main_rg.py
+++ report-generator/main_rg.py
@@ -26,7 +26,11 @@ def api():
     frm = tz_corrector.get_string(utc_frm)
     to = tz_corrector.get_string(utc_to)
     variables = {
+        'page-size': 'A4',
+        'margin-top': '0.75in',
+        'margin-right': '0.75in',
         'margin-bottom': '0.75in',
+        'margin-left': '0.75in',
         'footer-right': '[page]',
         'frm': frm,
         'to': to,
