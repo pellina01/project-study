9ac1c6bdc08743a14526ceaaa083c54febbbcecd
unknown <pellina01@gmail.com>
2021-02-22 18:17:49 +0800

    edit new path

--- rpi-code/feedback.py
+++ rpi-code/feedback.py
@@ -75,12 +75,13 @@ if __name__ == "__main__":
 
     switch = serialize(read_arduino, 11, 4, 5)
     sensor_function = sensor_func(read_do, 11, 3)
-    aerator = feedback(raspi["mqtt_url"], sensor_function,
-                       switch, 4, "aerator")
+    print(sensor_function())
+    # aerator = feedback(raspi["mqtt_url"], sensor_function,
+    #                    switch, 4, "aerator")
                        
-    while True:
-        try:
-            time.sleep(aerator.check())
-        except:
-            logging.error(traceback.format_exc())
-            print(traceback.format_exc())
+    # while True:
+    #     try:
+    #         time.sleep(aerator.check())
+    #     except:
+    #         logging.error(traceback.format_exc())
+    #         print(traceback.format_exc())
