693569e7485c3a324c333eff1a47b1a41e3b7929
geromedumbrigue <gerome.dumbrigue@tup.edu.ph>
2021-02-27 21:12:49 +0800

    added do lower limit

--- rpi-code/i2c.py
+++ rpi-code/i2c.py
@@ -1,5 +1,7 @@
-import smbus2 as smbus
+# import smbus2 as smbus
 
+
+from smbus2 import SMBus 
 # sensor type 1 for ph , 2 for turbidity
 MEMORY_ADDR = 0x00
 BYTE_LEN = 25
@@ -13,14 +15,25 @@ def convert_bytes_to_list(src):
     return convert
 
 
+# def read_arduino(slave_addr, sensor_type):
+#     try:
+#         I2Cbus = smbus.SMBus(BUS)
+#         byte = convert_bytes_to_list(bytes(str(sensor_type), "utf-8"))
+#         I2Cbus.write_i2c_block_data(slave_addr, MEMORY_ADDR, byte)
+#         response = I2Cbus.read_i2c_block_data(
+#             slave_addr, MEMORY_ADDR, BYTE_LEN)
+#         return "ok", float(bytearray(response).decode("utf-8", "ignore"))
+#     except Exception as e:
+#         print("failed to retrieve data from arduino...")
+#         print(e)
+#         return "error", e
+
 def read_arduino(slave_addr, sensor_type):
     try:
-        I2Cbus = smbus.SMBus(BUS)
-        byte = convert_bytes_to_list(bytes(str(sensor_type), "utf-8"))
-        I2Cbus.write_i2c_block_data(slave_addr, MEMORY_ADDR, byte)
-        response = I2Cbus.read_i2c_block_data(
-            slave_addr, MEMORY_ADDR, BYTE_LEN)
-        return "ok", float(bytearray(response).decode("utf-8", "ignore"))
+        SMBus(BUS).write_i2c_block_data(
+            slave_addr, MEMORY_ADDR, convert_bytes_to_list(bytes(str(sensor_type), "utf-8")))
+        return "ok", float(bytearray(SMBus(BUS).read_i2c_block_data(
+            slave_addr, MEMORY_ADDR, BYTE_LEN)).decode("utf-8", "ignore"))
     except Exception as e:
         print("failed to retrieve data from arduino...")
         print(e)
