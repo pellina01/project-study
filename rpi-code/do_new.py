798b875433774d6a17d87ffd60b66d8d603685fe
pellina01 <pellina01@github.com>
2021-03-03 17:14:26 +0800

    add do static

--- rpi-code/do_new.py
+++ rpi-code/do_new.py
@@ -28,3 +28,6 @@ def read_do(slave_addr, sensor_type):
         return "ok", round(adc_voltage * do_table[rounded_temp] / V_saturation, 2)
     except Exception as e:
         return "error", e
+
+if __name__ == "__main__":
+	print(read_do(11,3))
