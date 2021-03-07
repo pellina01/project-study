4be01e9416cc4fca36496a108df7b6a72f68778e
geromedumbrigue <gerome.dumbrigue@tup.edu.ph>
2021-03-07 15:12:15 +0800

    add smbus close

--- rpi-code/i2c.py
+++ rpi-code/i2c.py
@@ -1,7 +1,7 @@
 # import smbus2 as smbus
 
 
-import smbus2 as smbus
+from smbus2 import SMBus
 # sensor type 1 for ph , 2 for turbidity
 MEMORY_ADDR = 0x00
 BYTE_LEN = 25
@@ -28,12 +28,12 @@ def convert_bytes_to_list(src):
 
 def read_arduino(slave_addr, sensor_type):
     try:
-        I2Cbus = smbus.SMBus(BUS)
+        I2Cbus = SMBus(BUS)
         byte = convert_bytes_to_list(bytes(str(sensor_type), "utf-8"))
         I2Cbus.write_i2c_block_data(slave_addr, MEMORY_ADDR, byte)
         response = I2Cbus.read_i2c_block_data(
             slave_addr, MEMORY_ADDR, BYTE_LEN)
-        smbus.close()
+        I2Cbus.close()
         return "ok", float(bytearray(response).decode("utf-8", "ignore"))
     except Exception as e:
         print("failed to retrieve data from arduino...")
