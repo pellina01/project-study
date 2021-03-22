73374f5713b0c208060241ccb2e3f80e751ff66d
geromedumbrigue <gerome.dumbrigue@tup.edu.ph>
2021-03-22 15:53:37 +0800

    edit tz corrector format

--- report-generator/tz_correction.py
+++ report-generator/tz_correction.py
@@ -6,14 +6,16 @@ class tz_correction:
         self.tzone = self.pytz.timezone(timezone)
 
     def get_string(self, time_in_z):
-        unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
+        # unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
+        unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%SZ')
         utc_aware = unaware.replace(tzinfo=self.pytz.utc)
         tz_aware = utc_aware.astimezone(self.tzone)
 
         return tz_aware.strftime('%Y-%m-%d %I:%M %p')
 
     def get_datetime(self, time_in_z):
-        unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
+        # unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%S.%fZ')
+        unaware = self.datetime.strptime(time_in_z, '%Y-%m-%dT%H:%M:%SZ')
         utc_aware = unaware.replace(tzinfo=self.pytz.utc)
         return utc_aware.astimezone(self.tzone)
 
