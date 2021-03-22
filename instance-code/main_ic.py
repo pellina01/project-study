7e9c9ec99528879ce39ec740ced35528ba56802d
geromedumbrigue <gerome.dumbrigue@tup.edu.ph>
2021-03-22 15:25:27 +0800

    rename mqtt on cloud instance and comment out aggregator

--- instance-code/main_ic.py
+++ instance-code/main_ic.py
@@ -19,11 +19,11 @@ logging.basicConfig(filename=cloud["error_file"])
 
 registered_topics = []
 for topic in cloud["topics"]:
-    registered_topics.append(listen(topic, cloud["url"], cloud["influxHost"],
+    registered_topics.append(listen(topic, cloud["mqtt_url"], cloud["influxHost"],
                           cloud["database"], cloud["username"], cloud["password"]))
 calibrate = []
 for items in cloud["calibration"]:
-    calibrate.append(listen(items, cloud["url"], cloud["influxHost"],
+    calibrate.append(listen(items, cloud["mqtt_url"], cloud["influxHost"],
         cloud["database"], cloud["username"], cloud["password"], type="calibration"))
 
 switch =   { "ph": "pH",
@@ -32,19 +32,21 @@ switch =   { "ph": "pH",
             "do": "mg/L"
             }
        
-data_to_aggregate = []
-for topic in cloud["topics_aggregated"]:
-    data_to_aggregate.append(aggregator(topic, cloud["influxHost"], cloud["username"],
-            cloud["password"], cloud["database"], cloud["database_aggregated"], switch.get(topic, "no unit")))
+ #for aggregating aggregator      
+# data_to_aggregate = []
+# for topic in cloud["topics_aggregated"]:
+#     data_to_aggregate.append(aggregator(topic, cloud["influxHost"], cloud["username"],
+#             cloud["password"], cloud["database"], cloud["database_aggregated"], switch.get(topic, "no unit")))
 
-def aggregating():
-    for data in data_to_aggregate:
-        data.aggregate()
+#for aggregating
+# def aggregating():
+#     for data in data_to_aggregate:
+#         data.aggregate()
 
 
 
-# schedule.every(1).minutes.do(aggregating)
-schedule.every().day.at("23:59").do(aggregating)
+# schedule.every(1).minutes.do(aggregating) # aggregating sample(not included)
+# schedule.every().day.at("23:59").do(aggregating) #for aggregating
 
 listening = True
 while True:
