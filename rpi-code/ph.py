9c58ed11bf84de64e69cc125e7566b6547c8616a
geromedumbrigue <gerome.dumbrigue@tup.edu.ph>
2021-03-23 23:46:53 +0800

    edit arduino and i2c code

--- rpi-code/ph.py
+++ rpi-code/ph.py
@@ -4,10 +4,7 @@ D = 1.346884
 
 def read_ph(read_arduino, slave_addr, sensor_type, *args):
     try:
-        unit_list = []
-        for i in range(0,10):
-            unit_list.append(read_arduino(slave_addr, sensor_type)[1])
-        unit_summation = sum(unit_list)
+        unit_summation = read_arduino(slave_addr, sensor_type)[1]
         unit_ave = unit_summation/10
         return "ok", round((A*unit_ave) - D, 2)
     except Exception as e:
