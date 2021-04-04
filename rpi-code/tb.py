5d5f29f4e793b129c787cfb31f75424068c9f4ca
pellina01 <pellina01@github.com>
2021-04-04 17:42:47 +0800

    modify turbidity

--- rpi-code/tb.py
+++ rpi-code/tb.py
@@ -6,12 +6,14 @@ def read_tb(read_arduino, slave_addr, sensor_type, *args):
         volt =  raw * 5 / 1024
         print("tb raw: ", raw)
         print("tb volt: ", volt)
-        if volt < 2.5:
-            ntu = 3000.0
-        elif volt > 4.19:
-            ntu = 10.0
-        else:
-            ntu = -1120.4 * pow(volt, 2) + (5742.3 * volt) - 4353.8
+        # if volt < 2.5:
+        #     ntu = 3000.0
+        # elif volt > 4.19:
+        #     ntu = 10.0
+        # else:
+        #     ntu = -1120.4 * pow(volt, 2) + (5742.3 * volt) - 4353.8
+
+        ntu = 4999.25 - (1250 * volt)
         return "ok", round(ntu,2)
     except:
         return "error", traceback.format_exc()
