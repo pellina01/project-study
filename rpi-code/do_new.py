b2f312d8ca984773e2e70fef7736f1a38e326fb8
geromedumbrigue <gerome.dumbrigue@tup.edu.ph>
2021-06-27 16:44:54 +0800

    revised do

--- rpi-code/do_new.py
+++ rpi-code/do_new.py
@@ -15,21 +15,20 @@ do_table = [14.460, 14.220, 13.820, 13.440, 13.090, 12.740, 12.420, 12.110, 11.8
             9.080, 8.900, 8.730, 8.570, 8.410, 8.250, 8.110, 7.960, 7.820, 7.690,
             7.560, 7.430, 7.300, 7.180, 7.070, 6.950, 6.840, 6.730, 6.630, 6.530, 6.410]  # 0C to 41C only
 
-# def read_do(read_arduino, slave_addr, sensor_type, *args):
-#     try:
-#         adc_raw = read_arduino(slave_addr, sensor_type)[1]
-#         adc_voltage = round(adc_raw)*reference/resolution
-#         print("adc raw DO: ", adc_raw)
-#         print("mv: ", adc_voltage)
-#         temp = read_temp()
-#         rounded_temp = round(temp[1])
-#         V_saturation = (temp[1] - cal2_t) * (cal1_v -
-#                                              cal2_v) / (cal1_t - cal2_t) + cal2_v
-#         print("do" , round(adc_voltage * do_table[rounded_temp] / V_saturation, 2))
-#         return "ok", round(adc_voltage * do_table[rounded_temp] / V_saturation, 2)
-#     except:
-#         print(traceback.format_exc())
-#         return "error", traceback.format_exc()
+def read_do(read_arduino, slave_addr, sensor_type, *args):
+    try:
+        adc_raw = read_arduino(slave_addr, sensor_type)[1]
+        adc_voltage = round(adc_raw)*reference/resolution
+        print("adc raw DO: ", adc_raw)
+        print("mv: ", adc_voltage)
+        rounded_temp = round(read_temp()[1])
+        V_saturation = (temp[1] - cal2_t) * (cal1_v -
+                                             cal2_v) / (cal1_t - cal2_t) + cal2_v
+        print("do" , round(adc_voltage * do_table[rounded_temp] / V_saturation, 2))
+        return "ok", round(adc_voltage * do_table[rounded_temp] / V_saturation, 2)
+    except:
+        print(traceback.format_exc())
+        return "error", traceback.format_exc()
 
 
 # do_table = [14460, 14220, 13820, 13440, 13090, 12740, 12420, 12110, 11810, 11530,
@@ -37,20 +36,20 @@ do_table = [14.460, 14.220, 13.820, 13.440, 13.090, 12.740, 12.420, 12.110, 11.8
 #     9080, 8900, 8730, 8570, 8410, 8250, 8110, 7960, 7820, 7690,
 #     7560, 7430, 7300, 7180, 7070, 6950, 6840, 6730, 6630, 6530, 6410]
 
-def read_do(read_arduino, slave_addr, sensor_type, *args):
-    try:
-        adc_raw = read_arduino(slave_addr, sensor_type)[1]
-        adc_voltage = round(adc_raw)*reference/resolution
-        print("adc raw DO: ", adc_raw)
-        print("mv: ", adc_voltage)
-        temp = read_temp()
-        rounded_temp =  round(temp[1])
-        V_saturation =  cal1_v + (35 * temp[1]) - (cal1_v  * 35);
-        print("do" , round(adc_voltage * do_table[rounded_temp] / V_saturation, 2))
-        return "ok", round(adc_voltage * do_table[rounded_temp] / V_saturation, 2)
-    except:
-        print(traceback.format_exc())
-        return "error", traceback.format_exc()
+# def read_do(read_arduino, slave_addr, sensor_type, *args):
+#     try:
+#         adc_raw = read_arduino(slave_addr, sensor_type)[1]
+#         adc_voltage = round(adc_raw)*reference/resolution
+#         print("adc raw DO: ", adc_raw)
+#         print("mv: ", adc_voltage)
+#         temp = read_temp()
+#         rounded_temp =  round(temp[1])
+#         V_saturation =  cal1_v + (35 * temp[1]) - (cal1_v  * 35);
+#         print("do" , round(adc_voltage * do_table[rounded_temp] / V_saturation, 2))
+#         return "ok", round(adc_voltage * do_table[rounded_temp] / V_saturation, 2)
+#     except:
+#         print(traceback.format_exc())
+#         return "error", traceback.format_exc()
 
 
 
